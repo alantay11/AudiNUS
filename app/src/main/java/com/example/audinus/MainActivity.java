37d52138a805dcde264704bcbec1fdf9bc4b2c68
alantay11 <alantay1111@gmail.com>
2022-05-17 13:42:17 +0800

    add permission request and partial library view implementation

--- app/src/main/java/com/example/audinus/MainActivity.java
+++ app/src/main/java/com/example/audinus/MainActivity.java
@@ -1,25 +1,91 @@
 package com.example.audinus;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.os.Bundle;
 
 import android.media.MediaPlayer;
+import android.provider.MediaStore;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    RecyclerView recyclerView;
+    TextView noMusicTextView;
+    ArrayList<AudioModel> songList = new ArrayList<>();
 
     MediaPlayer music;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        recyclerView = findViewById(R.id.recycler_view);
+        noMusicTextView = findViewById(R.id.no_songs_text);
+
+        if(!checkPermission()) {
+            requestPermission();
+            //return;
+        }
+
+        String[] projection = {
+                MediaStore.Audio.Media.TITLE,
+                MediaStore.Audio.Media.DATA,
+                MediaStore.Audio.Media.DURATION,
+        };
+
+        String selection = MediaStore.Audio.Media.IS_MUSIC + " != 0";
+
+        Cursor cursor = getContentResolver().query(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
+                projection, selection, null, null);
+
+        while (cursor.moveToNext()) {
+            AudioModel songData = new AudioModel(cursor.getString(1), cursor.getString(0), cursor.getString(2));
+
+            if(new File(songData.getPath()).exists()) {
+                songList.add(songData);
+            }
+
+            if (songList.isEmpty()) {
+                noMusicTextView.setVisibility(View.VISIBLE);
+            } else {
+                recyclerView.setLayoutManager(new LinearLayoutManager(this));
+                recyclerView.setAdapter(new MusicListAdapter(songList, this));
+            }
+        }
+
         music = MediaPlayer.create(
-                this, R.raw.miraihenokioku);
+                this, R.raw.song);
     }
+
+    boolean checkPermission() {
+        return ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE) ==
+        PackageManager.PERMISSION_GRANTED;
+    }
+
+    void requestPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
+            Toast.makeText(MainActivity.this, "Read permissions are required, please enable in settings", Toast.LENGTH_SHORT).show();
+        } else {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+        }
+    }
+
+
 
     public void playSong(View v){
         music.start();
@@ -31,7 +97,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void stopSong(View v) {
         music.stop();
-        music = MediaPlayer.create(this, R.raw.miraihenokioku);
+        music = MediaPlayer.create(this, R.raw.song);
     }
 
 
