7442988b4e74ce2d1ecdda915f313aaa94f6406d
jingyulim1 <jingyulim1@gmail.com>
2022-06-10 17:08:36 +0800

    view on song list shouldn't repeat after switching fragments

--- app/src/main/java/com/orbital/audinus/SongsFragment.java
+++ app/src/main/java/com/orbital/audinus/SongsFragment.java
@@ -29,48 +29,49 @@ public class SongsFragment extends Fragment {
     private ArrayList<AudioModel> searchList;
     private LinearLayoutManager layoutManager;
     private SearchView searchView;
+    View rootView;
 
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        if (rootView == null) {
 
-        View rootView = inflater.inflate(R.layout.fragment_songs, container, false);
+            rootView = inflater.inflate(R.layout.fragment_songs, container, false);
 
-        recyclerView = rootView.findViewById(R.id.recycler_view);
-        TextView noMusicTextView = rootView.findViewById(R.id.no_songs_text);
-        searchView = rootView.findViewById(R.id.search_bar);
-        searchView.clearFocus();
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                return false;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String newText) {
-                ArrayList<AudioModel> filteredList = new ArrayList<>();
-                for (AudioModel x : songList){
-                    if (x.getTitle().toLowerCase().contains(newText.toLowerCase())){
-                        filteredList.add(x);
-                    }
+            recyclerView = rootView.findViewById(R.id.recycler_view);
+            TextView noMusicTextView = rootView.findViewById(R.id.no_songs_text);
+            searchView = rootView.findViewById(R.id.search_bar);
+            searchView.clearFocus();
+            searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+                @Override
+                public boolean onQueryTextSubmit(String query) {
+                    return false;
                 }
-                recyclerView.setAdapter(new MusicListAdapter(filteredList, getActivity()));
-                return false;
-            }
-        });
+
+                @Override
+                public boolean onQueryTextChange(String newText) {
+                    ArrayList<AudioModel> filteredList = new ArrayList<>();
+                    for (AudioModel x : songList) {
+                        if (x.getTitle().toLowerCase().contains(newText.toLowerCase())) {
+                            filteredList.add(x);
+                        }
+                    }
+                    recyclerView.setAdapter(new MusicListAdapter(filteredList, getActivity()));
+                    return false;
+                }
+            });
 
 
+            layoutManager = new LinearLayoutManager(rootView.getContext());
 
-        layoutManager = new LinearLayoutManager(rootView.getContext());
 
-
-        Uri uri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
-        String[] projection = {MediaStore.Audio.AudioColumns.TITLE,
-                MediaStore.Audio.AudioColumns.DATA,
-                MediaStore.Audio.AudioColumns.DURATION,
-                MediaStore.Audio.AudioColumns.ALBUM_ID};
+            Uri uri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+            String[] projection = {MediaStore.Audio.AudioColumns.TITLE,
+                    MediaStore.Audio.AudioColumns.DATA,
+                    MediaStore.Audio.AudioColumns.DURATION,
+                    MediaStore.Audio.AudioColumns.ALBUM_ID};
 
 
         /*String[] projection = {
@@ -79,34 +80,33 @@ public class SongsFragment extends Fragment {
                 MediaStore.Audio.Media.DURATION,
                 MediaStore.Audio.Albums._ID
         };*/
-        //String selection = MediaStore.Audio.Media.IS_MUSIC + " != 0";
+            //String selection = MediaStore.Audio.Media.IS_MUSIC + " != 0";
 
 
-        Cursor cursor = requireActivity().getContentResolver().query(uri,
-                projection, null, null, null);
+            Cursor cursor = requireActivity().getContentResolver().query(uri,
+                    projection, null, null, null);
 
 
-        while (cursor.moveToNext()) {
-            long albumID = cursor.getLong(3);
-            String albumArt = "content://media/external/audio/albumart/"+ albumID;
+            while (cursor.moveToNext()) {
+                long albumID = cursor.getLong(3);
+                String albumArt = "content://media/external/audio/albumart/" + albumID;
 
-            AudioModel songData = new AudioModel(cursor.getString(1), cursor.getString(0), cursor.getString(2), albumArt);
+                AudioModel songData = new AudioModel(cursor.getString(1), cursor.getString(0), cursor.getString(2), albumArt);
 
-            if (new File(songData.getPath()).exists()) {
-                songList.add(songData);
+                if (new File(songData.getPath()).exists()) {
+                    songList.add(songData);
+                }
+
+                if (songList.isEmpty()) {
+                    noMusicTextView.setVisibility(View.VISIBLE);
+                } else {
+                    recyclerView.setLayoutManager(layoutManager);
+                    recyclerView.setAdapter(new MusicListAdapter(songList, getActivity()));
+                }
             }
+            cursor.close();
 
-            if (songList.isEmpty()) {
-                noMusicTextView.setVisibility(View.VISIBLE);
-            } else {
-                recyclerView.setLayoutManager(layoutManager);
-                recyclerView.setAdapter(new MusicListAdapter(songList, getActivity()));
-            }
         }
-        cursor.close();
-
-
-
-        return rootView;
+            return rootView;
     }
 }
